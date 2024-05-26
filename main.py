a704b8fef8957ad67d3409fa147aa2bd8a7736d7
apolloailabs <luckywayne88@icloud.com>
2024-05-26 19:16:51 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,6 @@
+print("Welcome to Apollo Ai")
+user_selection = input("type 1 to use the number recognition neural network")
+if(user_selection == "1"):
+    print("number recognition is a work in progress :)") 
+else:
+    print("thats either not one of our neural nets or youre typing giberish :D")
