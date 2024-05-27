a8d3e6166bee7ce9e4c0a2bc70fdcd68774a4500
apolloailabs <luckywayne88@icloud.com>
2024-05-27 19:17:29 -0400

    removed prototype menu (will probably be re-added later) and added algebraic linear regression model

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
-print("Welcome to Apollo Ai")
-user_selection = input("type 1 to use the number recognition neural network")
-if(user_selection == "1"):
-    print("number recognition is a work in progress :)") 
-else:
-    print("thats either not one of our neural nets or youre typing giberish :D")
+# lukes notes
+# this program uses a linear regression model, which is pretty complicated
+# to summarize it basically predicts the value of a variable based off of the value of another
+
+
+from random import randint
+TRAIN_SET_LIMIT = 1000
+TRAIN_SET_COUNT = 100
+
+TRAIN_INPUT = list()
+TRAIN_OUTPUT = list()
+for i in range(TRAIN_SET_COUNT):
+    a = randint(0, TRAIN_SET_LIMIT)
+    b = randint(0, TRAIN_SET_LIMIT)
+    c = randint(0, TRAIN_SET_LIMIT)
+    op = a + (2*b) + (3*c)
+    TRAIN_INPUT.append([a, b, c])
+    TRAIN_OUTPUT.append(op)
+
+from sklearn.linear_model import LinearRegression
+
+predictor = LinearRegression(n_jobs=-1)
+predictor.fit(X=TRAIN_INPUT, y=TRAIN_OUTPUT)
+
+X_TEST = [[3, 6, 9]]
+outcome = predictor.predict(X=X_TEST)
+coefficients = predictor.coef_
+
+print('Outcome : {}\nCoefficients : {}'.format(outcome, coefficients))
